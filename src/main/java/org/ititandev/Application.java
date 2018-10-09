97d27b182b0a7517a31ed1bd6f818c45358b7a77
ITitanDev <manhpcpro@gmail.com>
2018-10-09 13:07:01 +0700

    change port & rename project

--- src/main/java/org/ititandev/Application.java
+++ src/main/java/org/ititandev/Application.java
@@ -3,10 +3,12 @@ package org.ititandev;
 import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
 	public static ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
